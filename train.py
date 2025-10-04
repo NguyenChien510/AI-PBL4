1b66ab4b10602865fd9084226c428544d7948f32
Nguyen Le Minh Chien <minhchienvadori@gmail.com>
2025-10-04 11:49:34 +0700

    model

--- train.py
+++ train.py
@@ -1,4 +1,14 @@
 #Train with Python API
 from ultralytics import YOLO
-model = YOLO("yolov8n.pt")
-model.train(data="datasets/dataset.yaml", epochs=50, imgsz=640)
+if __name__ == "__main__":
+    # Load model local
+    model = YOLO(r"D:\AI-PBL4\models\yolov8n.pt")
+
+    # Train 
+    model.train(
+        data=r"D:\AI-PBL4\datasets\dataset.yaml",
+        epochs=50,
+        imgsz=640,
+        batch=16,
+        name="train_local"
+    )
